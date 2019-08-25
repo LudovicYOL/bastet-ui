85eeff4813ac94a790fa843915eb371174fe7683
Ludovic YOL <ludovic.yol@gmail.com>
2019-08-25 23:46:24 +0200

    adding update password feature

--- src/app/components/settings/update-password/update-password.component.ts
+++ src/app/components/settings/update-password/update-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-update-password',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatePasswordComponent implements OnInit {
 
-  constructor() { }
+  actualPassword = "";
+  newPassword = "";
+  newPasswordBis = "";
+
+  constructor(public dialogRef: MatDialogRef<UpdatePasswordComponent>,
+    private authenticationService: AuthenticationService,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  update(): void{
+    if (this.actualPassword !== '' && this.newPassword !== '' && this.newPasswordBis !== '') {
+      if(this.newPassword != this.newPasswordBis){
+        this.snackBar.open('Les mots de passe ne correspondent pas', 'FERMER', {
+          duration: 3000,
+        });
+      }else{
+        let account = {
+          id: this.authenticationService.getUserDetails()._id,
+          actualPassword : this.actualPassword,
+          newPassword : this.newPassword,
+        }
+        this.authenticationService.updatePassword(account).subscribe((data)=>{
+          this.authenticationService.logout();
+        }, (err) => {
+          this.snackBar.open('Mot de passe actuel invalide', 'FERMER', {
+            duration: 3000,
+          });
+        });
+      }
+    } else {
+      this.snackBar.open('Veuillez remplir tous les champs', 'FERMER', {
+        duration: 3000,
+      });
+    }
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
   }
 
 }
