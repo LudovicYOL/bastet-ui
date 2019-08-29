fb8a213d112a97e2ffe0c749b33db417394ca422
Ludovic Yol <ludovic.yol@gmail.com>
2019-08-29 18:54:38 +0200

    fix filecase

---
+++ src/app/models/mission.model.ts
@@ -0,0 +1,10 @@
+export interface Mission {
+    _id: string;
+    poste: string;
+    enterprise: string;
+    domain: string;
+    startMonth: string;
+    endMonth: string;
+    startYear: number;
+    endYear: number;
+}
